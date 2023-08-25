aad08d6bfc4148f8b320796206ee88b87d4bf155
Ravi Bagdi <ravibagdi799@outlook.com>
2023-08-25 11:35:36 +0530

    change python version in lock file

--- tasteswift/settings.py
+++ tasteswift/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_8twz8iuciw5+4s2zuk3nxz4ip^kwy&wipu9hur5g^&r*_3*ab'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
